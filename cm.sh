2d8033ecf64892be7a319de23b512e2003c52d80
Doeke Zanstra <doekman@icloud.com>
2021-07-06 10:54:02 +0200

    Polish + child folder check

--- cm.sh
+++ cm.sh
@@ -8,12 +8,13 @@ function usage {
 Usage: $0 COMMAND [FLAGS] ...
 
 COMMAND:
+	cm init             Places a .config-man file in the current folder
 	cm add FILE [...]   Add one or more files to the vault
 	cm backup           Perform the backup
-	cm compare          List all changed files
-	cm init             Places a .config-man file in the current folder
 	cm list             Shows all files in the vault
+	cm compare          List all changed files
 	cm restore          (not-implemented-yet)
+	cm help             Show this page
 FLAGS:
 	-v, --verbose       Also print messages when something succeeds
 	-dr, --dry-run      Don't actually modify the file system
@@ -21,8 +22,10 @@ FLAGS:
 You will need to run this tool from a config-man vault.
 
 TOTHIERENNIETVERDER
-	echo "$(tput bel)❗️" "$@"
-	exit 1
+	if [[ $# -gt 0 ]]; then
+		echo "$(tput bel)❗️" "$@"
+		exit 1
+	fi
 }
 
 function verbose_echo {
@@ -32,22 +35,33 @@ function verbose_echo {
 }
 
 function cm_init {
-	local check_path
+	local check_path children
 	check_path="$(pwd)"
 	# Check if current and parent-folders are not a config-man base (cm repository can't be nested)
 	# Also, root-folder can't contain a .config-man file, so don't check.
 	while [[ $check_path != "/" ]]; do
-		if [[ -f "$check_path/.config-man" ]]; then
+		if [[ -f "$check_path/$CONFIG_FILE" ]]; then
 			if [[ $check_path == "$(pwd)" ]]; then
-				echo "ERROR: this folder is already initialized (the file '.config-man' exists)"
+				echo "ERROR: this folder is already initialized."
 			else
-				echo "ERROR: the parent-folder '$check_path' is already initialized (the file '.config-man' exists)"
+				echo "ERROR: a parent-folder is already initialized, folder:"
+				echo "$check_path"
 			fi
 			exit 1
 		fi
 		check_path="$(dirname "$check_path")"
 	done
-	[[ $DRY_RUN == 0 ]] && touch ./.config-man
+	# also check child-folders
+	set -f
+	# shellcheck disable=SC2207
+	children=( $(find . -name "$CONFIG_FILE" -type f) )
+	set +f
+	if [[ ${#children} -gt 0 ]]; then
+		echo "ERROR: at least one child-folder is already initialized, folder(s):"
+		echo "${children[*]}"
+		exit 2
+	fi
+	[[ $DRY_RUN == 0 ]] && touch "./$CONFIG_FILE"
 }
 
 function cm_add {
@@ -67,7 +81,7 @@ function cm_add {
 }
 
 function cm_list {
-	find "." -type f -not -name ".config-man" -and -not -name ".DS_Store" -and -not -path '*/\.git/*' | sed 's/^\.//'
+	find "." -type f -not -name "$CONFIG_FILE" -and -not -name ".DS_Store" -and -not -path '*/\.git/*' | sed 's/^\.//'
 }
 
 function cm_backup {
@@ -107,6 +121,7 @@ function cm_compare {
 }
 
 
+CONFIG_FILE=".config-man"
 DRY_RUN=0
 VERBOSE=0
 CM_BASE="$(pwd)"
@@ -116,8 +131,8 @@ if [[ ${1:-} == "init" ]]; then
 	exit 0
 fi
 
-if [[ ! -f ./.config-man ]]; then
-	usage "The file ./.config-man not found"
+if [[ ! -f ./$CONFIG_FILE ]]; then
+	usage "The file ./$CONFIG_FILE not found"
 fi
 
 # Command
@@ -125,7 +140,7 @@ if [[ $# -lt 1 ]]; then
 	usage "Please provide a command"
 fi
 case "$1" in
-	add|backup|compare|init|list|restore) cmd="$1";;
+	add|backup|compare|help|init|list|restore) cmd="$1";;
 	*) usage "Unknown command '$1'";;
 esac
 shift
@@ -140,7 +155,9 @@ while [[ $# -ge 1 ]]; do
 	esac
 done
 
-if [[ $cmd == "add" ]]; then
+if [[ $cmd == "help" ]]; then
+	usage
+elif [[ $cmd == "add" ]]; then
 	# Handle all FILES arguments
 	if [[ $# -lt 1 ]]; then
 		usage "You need to specify at least one file"
